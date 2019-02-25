ccb68349c665d3bbc26dcbfb10ea72c3e3d2b213
justin <justin.fournier@epitech.eu>
2019-02-25 01:18:10 +0100

    you can now play card locally

--- client/game/game.js
+++ client/game/game.js
@@ -13,9 +13,7 @@ function drag(ev) {
 function drop(ev) {
   ev.preventDefault();
   let data = ev.dataTransfer.getData("text");
-  ev.target.appendChild(document.getElementById(data));
-  document.getElementById(data).style.height = "14em";
-  document.getElementById(data).style.width = "9em";
+  document.getElementById('card-played').src = document.getElementById(data).src;
 }
 
 socket.on('hand', (hand) => {
