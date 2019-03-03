9d56bfbb944db184a4ab434777206da31f7a368a
justin <justin.fournier@epitech.eu>
2019-03-03 17:51:32 +0100

    dynamic enemy display

--- client/game/game.js
+++ client/game/game.js
@@ -2,6 +2,63 @@ const socket = require('electron').remote.require('./socket');
 
 let playerHand = [];
 let turn = false;
+let initialised = false;
+
+function createNewEnemyDisplay(isLeft, index) {
+  let content = `<div id="enemy-${index}-infos" class="box">
+      <div class="columns">
+        <div class="column" style="width: 15em">
+          Player 1
+        </div>
+        <div class="column" style="width: 15em">
+          Km traveled: <span id="enemy-${index}-score">0</span>
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-narrow">
+          Malus:
+        </div>
+        <div class="column is-narrow">
+          <img id="enemy-${index}-malus-1" src="" alt="card" style="width: auto; height: auto; resize: both; max-height: 3em">
+        </div>
+        <div class="column is-narrow">
+          <img id="enemy-${index}-malus-2" src="" alt="card" style="width: auto; height: auto; resize: both; max-height: 3em">
+        </div>
+        <div class="column is-narrow">
+          <img id="enemy-${index}-malus-3" src="" alt="card" style="width: auto; height: auto; resize: both; max-height: 3em">
+        </div>
+        <div class="column is-narrow">
+          <img id="enemy-${index}-malus-4" src="" alt="card" style="width: auto; height: auto; resize: both; max-height: 3em">
+        </div>
+        <div class="column is-narrow">
+          <img id="enemy-${index}-malus-5" src="" alt="card" style="width: auto; height: auto; resize: both; max-height: 3em">
+        </div>
+      </div>
+      <div class="columns">
+        <div class="column is-narrow">
+          Assets:
+        </div>
+        <div class="column is-narrow">
+          <img id="enemy-${index}-bonus-1" src="" alt="card" style="width: auto; height: auto; resize: both; max-height: 3em">
+        </div>
+        <div class="column is-narrow">
+          <img id="enemy-${index}-bonus-2" src="" alt="card" style="width: auto; height: auto; resize: both; max-height: 3em">
+        </div>
+        <div class="column is-narrow">
+          <img id="enemy-${index}-bonus-3" src="" alt="card" style="width: auto; height: auto; resize: both; max-height: 3em">
+        </div>
+        <div class="column is-narrow">
+          <img id="enemy-${index}-bonus-4" src="" alt="card" style="width: auto; height: auto; resize: both; max-height: 3em">
+        </div>
+      </div>
+    </div>`;
+
+  let side = 'left';
+  if (!isLeft) {
+    side = 'right';
+  }
+  document.getElementById(`${side}-enemy-panel`).innerHTML += content;
+}
 
 function allowDrop(ev) {
   ev.preventDefault();
@@ -44,7 +101,9 @@ socket.on('gameState', (playersDatas) => {
   console.log(playersDatas);
   let index = 1;
   for (const playerDatas of playersDatas) {
-    document.getElementById(`enemy-${index}-infos`).style.visibility = 'visible';
+    if (initialised === false) {
+      createNewEnemyDisplay(Boolean(index % 2), index);
+    }
     document.getElementById(`enemy-${index}-score`).innerText = playerDatas.score;
     for (const data of Object.entries(playerDatas.handicap)) {
       if (data[1] === true) {
@@ -54,6 +113,7 @@ socket.on('gameState', (playersDatas) => {
     index += 1;
   }
   document.getElementById(`self-score`).innerText = selfDatas.score;
+  initialised = true;
 });
 
 socket.emit('askHand');
