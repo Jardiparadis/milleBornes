118b3b476ead560e3b80d9011e145100a7ee6271
justin <justin.fournier@epitech.eu>
2019-03-08 16:06:45 +0100

    fullscreen and start script

--- client/index.js
+++ client/index.js
@@ -3,9 +3,8 @@ const { app, BrowserWindow } = require('electron');
 let win = null;
 
 function createWindow() {
-  win = new BrowserWindow({ width: 1600, height: 900, fullscreen: false , enter: true, resizable: false });
+  win = new BrowserWindow({ width: 1600, height: 900, fullscreen: true , enter: true, resizable: false });
   win.loadFile('./client/main_menu/main_menu.html');
-  win.webContents.openDevTools();
   win.on('closed', () => {
     win = null;
   })
